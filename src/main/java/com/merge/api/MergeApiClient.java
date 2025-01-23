ffff590d7a64f847c8a18aa695e0a925807d1912
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-23 22:39:16 +0000

    SDK regeneration

--- src/main/java/com/merge/api/MergeApiClient.java
+++ src/main/java/com/merge/api/MergeApiClient.java
@@ -16,46 +16,46 @@ import java.util.function.Supplier;
 public class MergeApiClient {
     protected final ClientOptions clientOptions;
 
-    protected final Supplier<CrmClient> crmClient;
-
     protected final Supplier<AtsClient> atsClient;
+
+    protected final Supplier<CrmClient> crmClient;
 
     protected final Supplier<FilestorageClient> filestorageClient;
 
-    protected final Supplier<HrisClient> hrisClient;
-
     protected final Supplier<TicketingClient> ticketingClient;
+
+    protected final Supplier<HrisClient> hrisClient;
 
     protected final Supplier<AccountingClient> accountingClient;
 
     public MergeApiClient(ClientOptions clientOptions) {
         this.clientOptions = clientOptions;
-        this.crmClient = Suppliers.memoize(() -> new CrmClient(clientOptions));
         this.atsClient = Suppliers.memoize(() -> new AtsClient(clientOptions));
+        this.crmClient = Suppliers.memoize(() -> new CrmClient(clientOptions));
         this.filestorageClient = Suppliers.memoize(() -> new FilestorageClient(clientOptions));
-        this.hrisClient = Suppliers.memoize(() -> new HrisClient(clientOptions));
         this.ticketingClient = Suppliers.memoize(() -> new TicketingClient(clientOptions));
+        this.hrisClient = Suppliers.memoize(() -> new HrisClient(clientOptions));
         this.accountingClient = Suppliers.memoize(() -> new AccountingClient(clientOptions));
-    }
-
-    public CrmClient crm() {
-        return this.crmClient.get();
     }
 
     public AtsClient ats() {
         return this.atsClient.get();
     }
 
+    public CrmClient crm() {
+        return this.crmClient.get();
+    }
+
     public FilestorageClient filestorage() {
         return this.filestorageClient.get();
     }
 
-    public HrisClient hris() {
-        return this.hrisClient.get();
-    }
-
     public TicketingClient ticketing() {
         return this.ticketingClient.get();
+    }
+
+    public HrisClient hris() {
+        return this.hrisClient.get();
     }
 
     public AccountingClient accounting() {
